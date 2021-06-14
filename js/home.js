df86e11da16d993163681497a6bd50d986e36158
attila <attilavigh.contact@gmail.com>
2021-06-14 20:08:33 +0100

    Section "Good morning! added.

--- js/home.js
+++ js/home.js
@@ -1,27 +1,18 @@
 import { spotifyData } from './spotifyData.js'
-import { spotifyDataLarge } from './spotifyDataLarge.js'
 
-console.log( spotifyData );
-console.log( spotifyDataLarge );
+const goodMorningCards = document.querySelector(".good-moring-cards")
 
-const goodMorning = document.querySelector("#good-moring")
+goodMorningCards.innerHTML =""
 
-goodMorning.innerHTML ="This is emptines"
-console.log( goodMorning.innerHTML);
+spotifyData.forEach ( spotify => {
 
-goodMorning.innerHTML +=`
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <img src=${ spotifyData[1].img } alt="...">
-                        </div>
-                        <div class="col-md-8 text-right">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-`
+    const { img, title } = spotify
+
+    goodMorningCards.innerHTML 
+        +=`
+            <div ">
+                <img src=${ img } alt="...">
+                <h5 class="card-title"> ${ title } </h5>
+            </div>
+        `
+})
