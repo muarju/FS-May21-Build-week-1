112b30ba53e8b0e953ae9b60cf2c1e41db3301e0
attila <attilavigh.contact@gmail.com>
2021-06-15 09:25:33 +0100

    Recenty played section added

--- js/showRunner.js
+++ js/showRunner.js
@@ -6,6 +6,7 @@ const goodMorningData = spotifyDataLarge.filter(spotifyData =>
     spotifyData.tag === "goodMorning"
 )
 
+//! ------------------------------------< Good Morning Section >-------------------------------------------
 const cardGoodMorning = spotify => `
                                         <div ">
                                             <img src=${ spotify.img } alt="...">    
@@ -13,3 +14,22 @@ const cardGoodMorning = spotify => `
                                         </div>
                                     `
 displayCards(".good-morning-cards", goodMorningData, cardGoodMorning )
+
+
+
+//! ------------------------------------< Recently played Section >-------------------------------------------
+
+const card = spotify =>`
+<div >
+    <img src=${ spotify.img } alt="...">
+    <h5 > ${ spotify.title } </h5>
+    <p ><small>Last updated </small></p>
+</div>
+` 
+
+displayCards(
+
+    ".recently-played-cards" 
+    , spotifyDataLarge.filter( spotifyData => spotifyData.tag === "recentlyPlayed" ) 
+    , card
+)
