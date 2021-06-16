239c88bf0b9dfb383115fa3a631e93f6ce541835
Engr. Md. Muslim Uddin Arju <mdarju240@gmail.com>
2021-06-16 13:38:53 +0200

    fix all responsive issues and cleanup raw css code

--- js/showRunner.js
+++ js/showRunner.js
@@ -7,23 +7,33 @@ const goodMorningData = spotifyDataLarge.filter(spotifyData =>
 )
 
 //! ------------------------------------< Good Morning Section >-------------------------------------------
-const cardGoodMorning = spotify => `
-                                        <div ">
-                                            <img src=${ spotify.img } alt="...">    
-                                            <h5 >    ${ spotify.title } </h5>
+const cardGoodMorning = spotify => `<div class="col">
+                                    <div class="card mb-3">
+                                    <div class="row no-gutters">
+                                        <div class="col-3">
+                                        <img src=${ spotify.img } alt="..." style="width:100%; height:auto">
                                         </div>
+                                        <div class="col-9">
+                                        <div class="card-body" style="padding:5px">
+                                            <h5 class="card-title text-truncate" style="font-size:15px">${ spotify.title }</h5>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    </div>
                                     `
-displayCards(".good-morning-cards", goodMorningData, cardGoodMorning )
-
-
-
+displayCards(".morning-cards", goodMorningData, cardGoodMorning )
 
 
 const card = spotify =>`
-<div >
+<div class="col">
+    <div class="card mb-3">
     <img src=${ spotify.img } alt="...">
-    <h5 > ${ spotify.title } </h5>
-    <p ><small>Last updated </small></p>
+    <div class="card-body">
+        <h5 class="card-title text-truncate">${ spotify.title } </h5>
+        <p ><small>Last updated </small></p>
+    </div>
+    </div>
 </div>
 ` 
 
